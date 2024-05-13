b330d730c8fbd89439c8c70f0121f9dbe2a04106
Huang-Xiao-Huang <40208954+Huang-Xiao-Huang@users.noreply.github.com>
2024-05-14 01:04:00 +0800

    Update 🏠 首页.py

--- 🏠 首页.py
+++ 🏠 首页.py
@@ -1,15 +1,59 @@
 import streamlit as st
 
+
 #==============首页信息==================
 chat = None
 #如果openai没有在会话中(会话的作用是存储一些历史信息),则设置为空
 if "OPENAI_API_KEY" not in st.session_state:
     st.session_state["OPENAI_API_KEY"] = ''
 
+
+
+
 # 页面设置
 st.set_page_config(page_title="AI 智能小工具",layout= "wide",page_icon="👀")
 st.title(":red[TOO] 酷智能工具")
 
+
+
+# =============banner============
+# 定义不同的 CSS 样式
+html = """
+<style>
+.metric-container-1 {
+    background-color: lightblue;
+    color: black; /* 设置字体颜色为黑色 */
+    padding: 15px;
+    border-radius: 10px;
+    box-shadow: 2px 2px 5px grey;
+}
+
+.metric-container-2 {
+    background-color: lightgreen;
+    color: black; /* 设置字体颜色为白色 */
+    padding: 15px;
+    border-radius: 10px;
+    box-shadow: 2px 2px 5px grey;
+}
+
+.metric-container-3 {
+    background-color: lightcoral;
+    color: black; /* 设置字体颜色为黑色 */
+    padding: 15px;
+    border-radius: 10px;
+    box-shadow: 2px 2px 5px grey;
+}
+
+.custom-text {
+    font-weight: bold;
+    color: #FF5733; /* 设置自定义颜色 */
+    font-size: 20px; /* 设置字体大小为20px */
+}
+</style>
+"""
+
+# 在页面上渲染自定义样式
+st.markdown(html, unsafe_allow_html=True)
 
 # 创建三个列
 col1, col2, col3 = st.columns(3)
@@ -24,6 +68,10 @@ with col3:
 
 
 
+
+# st.title(":red[TOO] 酷智能工具")
+#st.subheader(":blue[快捷 | 方便 | 准确]")
+
 #===============侧边栏信息============================
 with st.sidebar:
     st.subheader('TOO 酷')
@@ -32,6 +80,21 @@ with st.sidebar:
     st.write("💡 开发者: 哒哒er")
 
 
+# ============分割线样式=========
+st.markdown(
+    """
+    <style>
+    .custom-divider {
+        margin-top: 0px;
+        margin-bottom: 0px;
+        border-top: 1px solid red;
+        color:red;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+st.markdown('<div class="custom-divider"></div>', unsafe_allow_html=True)
 
 # 设置部分
 st.markdown("##### ⚙ 基本设置")
@@ -39,11 +102,29 @@ st.markdown("##### ⚙ 基本设置")
 col1,col2 = st.columns([3,1])
 with col1:
     openai_api_key = st.text_input(label= "请输入api_key",type="password")
-
     saved = st.button("💾 保存")
 
 
 
+    # 定义自定义 CSS 样式
+    html = """
+    <style>
+    .custom-button {
+        height: 50px; /* 设置按钮高度为50px */
+        line-height: 50px; /* 垂直居中文本 */
+        text-align: center; /* 文本水平居中 */
+        background-color: lightblue;
+        color: black;
+        font-size: 16px;
+        font-weight: bold;
+        border-radius: 5px;
+        cursor: pointer;
+    }
+    </style>
+    """
+
+    # 在页面上渲染自定义样式
+    st.markdown(html, unsafe_allow_html=True)
 
 
 
@@ -60,38 +141,66 @@ st.markdown('<div class="custom-divider"></div>', unsafe_allow_html=True)
 st.markdown("##### 🕶 挑选文字面具")
 col1, col2, col3,col4,col5,col6 = st.columns(6)
 with col1:
-    st.page_link("/pages/1_💬 智能对话.py", label=" 智能对话 ", icon="1️⃣")
+    st.page_link("pages/1_💬 智能对话.py", label=" 智能对话 ", icon="1️⃣")
 
 with col2:
-    st.page_link("/pages/2_📕 小红书文案.py", label="小红书文案 ", icon="2️⃣")
+    st.page_link("pages/2_📕 小红书文案.py", label="小红书文案 ", icon="2️⃣")
 
 with col3:
-    st.page_link("/pages/3_📺 视频文案.py", label=" 视频文案 ", icon="3️⃣")
+    st.page_link("pages/3_📺 视频文案.py", label=" 视频文案 ", icon="3️⃣")
 
 with col4:
-    st.page_link("/pages/4_📔 文档问答.py", label=" PDF问答 ", icon="4️⃣")
+    st.page_link("pages/4_📔 文档问答.py", label=" PDF问答 ", icon="4️⃣")
 
 with col5:
-    st.page_link("/pages/5_📁 文本摘要.py", label=" 文本摘要 ", icon="5️⃣")
+    st.page_link("pages/5_📁 文本摘要.py", label=" 文本摘要 ", icon="5️⃣")
 
 
 col6,col7, col8,col9,col10 = st.columns(5)
 with col6:
-    st.page_link("/pages/9_📝 简历书写.py", label="简历书写 ", icon="9️⃣")
+    st.page_link("./pages/8_📝 简历书写.py", label="简历书写 ", icon="9️⃣")
 
 st.markdown("##### 🕶 挑选图片面具")
 col11, col12, col13, col14, col15 = st.columns(5)
+
+
 with col11:
-    st.page_link("/pages/6_📷 识图工具.py", label=" 识图工具 ", icon="6️⃣")
+    st.page_link("pages/6_💟 图片描述.py"  ,label="图片描述", icon="7️⃣")
 
 with col12:
-    st.page_link("/pages/7_💟 图片描述.py"  ,label="图片描述", icon="7️⃣")
-
-with col13:
-    st.page_link("/pages/8_🖼 以文生图.py"  ,label="以文生图", icon="8️⃣")
+    st.page_link("pages/7_🖼 以文生图.py"  ,label="以文生图", icon="8️⃣")
 
 
+
+#============面具样式设计部分=============================
+# 面具部分的按钮
+button_style_image = """
+    <style>
+    [data-testid="stHorizontalBlock"] {
+        //background-color: #ccffff;
+        border-radius: 5px;
+        height:200%;
+        padding-left:10px;
+        padding-top:10px;
+        padding-bottom:10px;
+
+        
+    }
+    </style>
+"""
 st.markdown(button_style_image, unsafe_allow_html=True)
+#页面跳转按钮代码
+diliver_image = """
+    <style>
+    [data-testid="stPageLink-NavLink"] {
+        background-color:#66ccff;
+        height:45px;
+        box-shadow: 2px 2px 5px grey;
+    }
+    </style>
+"""
+st.markdown(diliver_image, unsafe_allow_html=True)
+
 #分割线条
 st.markdown('<div class="custom-divider"></div>', unsafe_allow_html=True)
 
