68c647c122cedc1291f97bda54287a9314b3a8d6
Huang-Xiao-Huang <40208954+Huang-Xiao-Huang@users.noreply.github.com>
2024-05-29 23:40:05 +0800

    Update 4_📔 文档问答.py

--- pages/4_📔 文档问答.py
+++ pages/4_📔 文档问答.py
@@ -54,7 +54,7 @@ if folder_path and input_questions and st.session_state["OPENAI_API_KEY"]:
         st.write(response["answer"])
         st.session_state["chat_history"] = response["chat_history"]
 else:
-    st.warning("请输入相关的信息)
+    st.warning("请输入相关的信息")
 
 
 if "chat_history" in st.session_state:
