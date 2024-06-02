971bc43eda6363e9ae35d19381900845b7cc005c
Huang-Xiao-Huang <40208954+Huang-Xiao-Huang@users.noreply.github.com>
2024-06-02 18:50:41 +0800

    Update shengtumodule.py

--- namemodule/shengtumodule.py
+++ namemodule/shengtumodule.py
@@ -1,4 +1,5 @@
-
+import requests
+import re
 #😚
 def shTu(client,number,Prompt):
     image_data = []
@@ -15,4 +16,25 @@ def shTu(client,number,Prompt):
     return image_data
             # print("Generated image:", image_url)
 
+def shiMID(client,number,Prompt):
+    image_data = []
+    url = "https://api.acedata.cloud/midjourney/imagine"
+    headers = {
+        "accept": "application/json",
+        "authorization": "Bearer e6d22c7afb4441f3b17a0a4ae5919079",
+        "content-type": "application/json"
+    }
+    for i in range(number):
+        payload = {
+            "mode": "fast",
+            "translation": True,
+            "prompt": Prompt
+        }
 
+        response = requests.post(url, json=payload, headers=headers).text
+        derw = re.compile('"image_url":"(.*?)"', re.S)
+        print(derw)
+        iamge_url = re.findall(derw, response)[0]
+        # print(e)
+        image_data.append(iamge_url)
+    return  image_data
