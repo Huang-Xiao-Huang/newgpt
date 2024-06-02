8194ef1ea9ef8e1a271acfc97f449f00ec45de39
Huang-Xiao-Huang <40208954+Huang-Xiao-Huang@users.noreply.github.com>
2024-06-02 19:50:37 +0800

    Update 7_💟 图片描述.py

--- pages/7_💟 图片描述.py
+++ pages/7_💟 图片描述.py
@@ -1,35 +1,35 @@
 from langchain_openai import  ChatOpenAI
 import streamlit as st
 import os
-#from PIL import Image
+from PIL import Image
 import sys
 
-
-
+sys.path.append('../namemodule')
+from namemodule import imagedescribe
 
 #图片分类
 if "OPENAI_API_KEY" not in st.session_state:
     st.session_state["OPENAI_API_KEY"] = ''
-    st.warning("请在首页输入你的api_key")
 elif st.session_state["OPENAI_API_KEY"] != '':
     chat = ChatOpenAI(openai_api_key=st.session_state["OPENAI_API_KEY"],
                                          openai_api_base="https://api.aigc369.com/v1")
-st.set_page_config(page_title="文档总结",layout="wide")
+st.set_page_config(page_title="图片描述",layout="wide")
 st.title("Too酷 :red[图片描述]")
 st.subheader("🤫 图片描述 🤫")
+chat = ChatOpenAI(openai_api_key=st.session_state["OPENAI_API_KEY"],
+                                         openai_api_base="https://api.aigc369.com/v1")
 
-folder_path = st.file_uploader(":red[🗂]上传图片", type=["PNG", "JPG"], help="支持文件类型图片",
-                                 label_visibility="visible")
-# folder_path = rf'{folder_path}'
-print(folder_path)
+def main():
+    folder_path = st.text_input("请输入图片链接")
+    if st.button("🖼 开始描述") and st.session_state["OPENAI_API_KEY"]:
+        st.image(folder_path)
+        with st.spinner("⌛图片描述生成中..."):
+            result = imagedescribe.im_describe(folder_path,st.session_state["OPENAI_API_KEY"])
+            st.text(result)
+    else:
+        st.warning("请在首页输入api_key")
+main()
 
-buttoned = st.button("🆗  开启描述")
 
-if buttoned :
-    # 读取文件夹中的文件
-    st.info("使用文档")
 
-    # except:pass
-else:
-    st.write('请输入文件夹路径。')
 
