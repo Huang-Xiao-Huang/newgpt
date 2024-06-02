fae28d890780a488ff8f35140f44c9a5a090d6e1
Huang-Xiao-Huang <40208954+Huang-Xiao-Huang@users.noreply.github.com>
2024-06-02 18:50:11 +0800

    Update 8_🖼 以文生图.py

--- pages/8_🖼 以文生图.py
+++ pages/8_🖼 以文生图.py
@@ -1,22 +1,34 @@
 from openai import OpenAI
 import streamlit as st
 
-
 import sys
 sys.path.append('../namemodule')
 from namemodule import shengtumodule
 
 
+def xianshitu(image_data):
+    '''
+    显示图片
+    '''
+    images_per_row = 5
+    image_spacing = 5
+    #显示多张图片，每行最多5张
+    for i in range(0, len(image_data), images_per_row):
+        row_images = image_data[i:i + images_per_row]  # 获取当前行的图片列表
+        row_captions = [f"Image {i + 1}" for i in range(len(row_images))]  # 设置当前行的图片标题
+        st.divider()
+        for img, caption in zip(row_images, row_captions):
+            st.image(img, caption=caption, width=150)  # 显示当前行的图片
+            # 添加下载按钮
+            download_button_str = f"Download {caption}"
+
+
 
 st.set_page_config(page_title="以文生图",layout="wide")
-
 st.title("Too酷 :red[以文生图]")
 st.subheader(":red[🖼] 智能图片生成")
 st.write("你想要的图片这里都有")
-
-
 st.divider() #分割线
-
 client = None
 #如果openai没有在会话中(会话的作用是存储一些历史信息),则设置为空
 if "OPENAI_API_KEY" not in st.session_state:
@@ -30,52 +42,47 @@ elif st.session_state["OPENAI_API_KEY"] != '':
 #                          base_url = "https://api.aigc369.com/v1")\
 
 
-if client:
-    options = st.multiselect(
-        '请你选择生成图像的风格',
-        ["插画风格", "黑白摄影风格", "自然风景风格",
-         "艺术风格", "明亮生动风格"],
-        ["艺术风格"]
-    )
 
-    st.write('你选择的是🤫:', options)
-    # print(options)
+model_image =  st.multiselect(
+    '选择模型与工具',
+    ["dall-e-2", "midjourney"],
+    ["dall-e-2"]
+)
+options = st.multiselect(
+    '请你选择生成图像的风格',
+    ["插画风格", "黑白摄影风格", "自然风景风格",
+     "艺术风格", "明亮生动风格"],
+    ["艺术风格"]
+)
 
-    number = st.number_input('🗒 生成的图片个数', value=1, min_value=1, max_value=4)
+st.write('你选择的是🤫:', options)
+# print(options)
 
-    Prompt12 = st.text_input("🎨 请输入图片描述")
-    bttons = st.button("💾按钮")
+number = st.number_input('🗒 生成的图片个数', value=1, min_value=1, max_value=4)
 
-    Prompt = f"你现在是一名著名画家,请画一幅{Prompt12}画,要求{options[0]}风格"
-    print(Prompt)
+Prompt12 = st.text_input("🎨 请输入图片描述")
+bttons = st.button("💾按钮")
 
-    if bttons:
-        try:
-            with st.spinner("⌛ 图片生成中..."):
+Prompt = f"你现在是一名著名画家,请画一幅{Prompt12}画,要求{options[0]}风格"
+print(Prompt)
 
-                image_data = shengtumodule.shTu(client,number,Prompt)
-                # for image in image_data:
-                #     st.image(image,width=200)
-                #     # 定义每行最多显示的图片数量
-                images_per_row = 5
-                image_spacing = 5
-                #显示多张图片，每行最多5张
-                for i in range(0, len(image_data), images_per_row):
-                    row_images = image_data[i:i + images_per_row]  # 获取当前行的图片列表
-                    row_captions = [f"Image {i + 1}" for i in range(len(row_images))]  # 设置当前行的图片标题
-                    st.divider()
-                    # st.image(row_images, caption=row_captions, width=150)  # 显示当前行的图片
-                    for img, caption in zip(row_images, row_captions):
-                        st.image(img, caption=caption, width=150)  # 显示当前行的图片
-                        # 添加下载按钮
-                        download_button_str = f"Download {caption}"
-                        # st.download_button(label=download_button_str, data=img, file_name=f"{caption}.jpg",
-                        #                    mime="image/jpeg")
+if bttons  and  st.session_state["OPENAI_API_KEY"]:
+    # try:
+    if model_image[0] == "dall-e-2":
+        with st.spinner("⌛ 图片生成中..."):
+            image_data = shengtumodule.shTu(client,number,Prompt)
+            xianshitu(image_data)
+    elif model_image[0] == "midjourney":
+        with st.spinner("⌛ 图片生成中..."):
+            image_data = shengtumodule.shiMID(client,number,Prompt)
+            xianshitu(image_data)
 
-        except:
-            st.warning("无法生成图片")
-            st.stop()
-    else:
-        pass
-else:
-    st.warning("请在首页输入api-key")
+
+                    # st.download_button(label=download_button_str, data=img, file_name=f"{caption}.jpg",
+                    #                    mime="image/jpeg")
+
+    # except:
+    #     st.warning("无法生成图片")
+    #     st.stop()
+
+
