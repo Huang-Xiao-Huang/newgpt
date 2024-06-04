77f05e77b6ce04b44417679cce85db3ec447e26a
Huang-Xiao-Huang <40208954+Huang-Xiao-Huang@users.noreply.github.com>
2024-06-04 15:57:01 +0800

    Update tranlatemodule.py

--- namemodule/tranlatemodule.py
+++ namemodule/tranlatemodule.py
@@ -7,8 +7,9 @@ def translate_text(text_to_translate,source_lang,target_lang,model):
     text_template = ChatPromptTemplate.from_messages(
         [
             ("human",
-             f"""你是一位出色的翻译官。请根据下面提示的文本进行翻译，实现准确的翻译。
-             要求将下列文本信息：{text_to_translate}，由{source_lang}翻译成{target_lang}
+             f"""你是一位出色的翻译官。请翻译下列句子
+             {text_to_translate}，
+             要求：由{source_lang}翻译成{target_lang}，翻译的句子要准确
             """)
         ]
     )
