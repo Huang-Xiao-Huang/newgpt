f5aafcf8eb533c6a9b63b403c363fdf27b25baa8
Huang-Xiao-Huang <40208954+Huang-Xiao-Huang@users.noreply.github.com>
2024-06-06 20:33:16 +0800

    Update 11_📈 分析工具.py

--- pages/11_📈 分析工具.py
+++ pages/11_📈 分析工具.py
@@ -35,32 +35,31 @@ def create_chart(input_data, chart_type):
     elif chart_type == "scatter":
         st.scatter_chart(df_data)
 
-
 data = st.file_uploader("上传你的数据文件（csv格式）：", type="csv")
 if data:
     st.session_state["df"] = pd.read_csv(data)
     with st.expander("源数据"):
         st.dataframe(st.session_state["df"])
 
-query = st.text_area("请输入你的问题：")
-button = st.button("生成回答")
+# query = st.text_area("请输入你的问题：")
+# button = st.button("生成回答")
 
-if button and not st.session_state["OPENAI_API_KEY"]:
-    st.info("请输入你的OpenAI API密钥")
-if button and "df" not in st.session_state:
-    st.info("请先上传数据文件")
-if button and st.session_state["OPENAI_API_KEY"] and "df" in st.session_state:
-    with st.spinner("AI正在思考中，请稍等..."):
-        response_dict = #dataframe_agent(st.session_state["OPENAI_API_KEY"], st.session_state["df"], query)
-        if "answer" in response_dict:
-            st.write(response_dict["answer"])
-        if "table" in response_dict:
-            st.table(pd.DataFrame(response_dict["table"]["data"],
-                                  columns=response_dict["table"]["columns"]))
-        if "bar" in response_dict:
-            create_chart(response_dict["bar"], "bar")
-        if "line" in response_dict:
-            create_chart(response_dict["line"], "line")
-        if "scatter" in response_dict:
-            create_chart(response_dict["scatter"], "scatter")
+# if button and not st.session_state["OPENAI_API_KEY"]:
+#     st.info("请输入你的OpenAI API密钥")
+# if button and "df" not in st.session_state:
+#     st.info("请先上传数据文件")
+# if button and st.session_state["OPENAI_API_KEY"] and "df" in st.session_state:
+#     with st.spinner("AI正在思考中，请稍等..."):
+#         response_dict = #dataframe_agent(st.session_state["OPENAI_API_KEY"], st.session_state["df"], query)
+#         if "answer" in response_dict:
+#             st.write(response_dict["answer"])
+#         if "table" in response_dict:
+#             st.table(pd.DataFrame(response_dict["table"]["data"],
+#                                   columns=response_dict["table"]["columns"]))
+#         if "bar" in response_dict:
+#             create_chart(response_dict["bar"], "bar")
+#         if "line" in response_dict:
+#             create_chart(response_dict["line"], "line")
+#         if "scatter" in response_dict:
+#             create_chart(response_dict["scatter"], "scatter")
 
